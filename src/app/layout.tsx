beec0fc48c2e7f05a92b0de9eda7f9e1fc9eba75
Tech suryanshsk <techsuryanshsk@gmail.com>
2025-12-02 23:53:08 +0000

    still favicon not updates i can see the firebase studio logo on the favi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,15 @@ const sourceCodePro = Source_Code_Pro({
   variable: '--font-source-code-pro',
 });
 
+const faviconUrl = 'https://instagram.fdel3-4.fna.fbcdn.net/v/t51.2885-19/499791407_18356727838194438_4860837939287100456_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.fdel3-4.fna.fbcdn.net&_nc_cat=106&_nc_oc=Q6cZ2QFjv8I--X8XK1X-fafYE7RNaKOXRVnbReeXo1DOqykepCQPVzDsqf40RZPBi8daxr1ZHjPAO-iDj1aI9iGxpKc_&_nc_ohc=zW9U4qSpvWYQ7kNvwEnJ_ds&_nc_gid=8urt1d8Xev56-4iMf1GKVw&edm=APoiHPcBAAAA&ccb=7-5&oh=00_AfmwyZPDZKdfF7_Xp1Bcv__fqWmUrEps-qWowsoNm-B2GA&oe=69355721&_nc_sid=22de04';
+
 export const metadata: Metadata = {
   title: 'Avanish.AI',
   description: 'Agentic &amp; Multimodal AI Engineer · Full Stack Web Developer · Content Creator',
   icons: {
-    icon: 'https://instagram.fdel3-4.fna.fbcdn.net/v/t51.2885-19/499791407_18356727838194438_4860837939287100456_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.fdel3-4.fna.fbcdn.net&_nc_cat=106&_nc_oc=Q6cZ2QFjv8I--X8XK1X-fafYE7RNaKOXRVnbReeXo1DOqykepCQPVzDsqf40RZPBi8daxr1ZHjPAO-iDj1aI9iGxpKc_&_nc_ohc=zW9U4qSpvWYQ7kNvwEnJ_ds&_nc_gid=8urt1d8Xev56-4iMf1GKVw&edm=APoiHPcBAAAA&ccb=7-5&oh=00_AfmwyZPDZKdfF7_Xp1Bcv__fqWmUrEps-qWowsoNm-B2GA&oe=69355721&_nc_sid=22de04',
+    icon: faviconUrl,
+    shortcut: faviconUrl,
+    apple: faviconUrl,
   },
 };
 
